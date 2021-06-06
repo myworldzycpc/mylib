e16af42b1266b361c67163af77005162400b6808
myworldzycpc <67212814+myworldzycpc@users.noreply.github.com>
2021-06-06 05:37:38 +0000

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,1 +1,9 @@
-print("haha")
+def testformny(index, money):
+    shouldmny = smtlst[index-1]["money"]
+    user = zzrlst[-1]["user"]
+    if money > shouldmny:
+        pass
+    if money < shouldmny:
+        zzrmoney(zzrmnys[index])
+    else:
+        startsz(zzrmnys["user"],smtlst[index-1]["count"])
